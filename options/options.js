25e1500b207214f2f8e8c8eed5922cad76f80224
Crowdbotics <team@crowdbotics.com>
2023-10-26 16:22:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sneaker-store-44156",
+  url: "https://sneaker-store-44156.botics.co",
+  api: "https://sneaker-store-44156.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
